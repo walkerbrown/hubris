3a268f0fcac1e41466e94207ff8320c090a6a980
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-01-29 19:52:15 -0500

    WIP - some humility and better ringbuf!

--- task/spdm-responder/src/main.rs
+++ task/spdm-responder/src/main.rs
@@ -5,13 +5,14 @@
 #![no_std]
 #![no_main]
 
-use idol_runtime::{RequestError, LenLimit, Leased, W, R};
+use idol_runtime::{Leased, LenLimit, RequestError, R, W};
 use userlib::*;
 
 use ringbuf::{ringbuf, ringbuf_entry};
 use spdm::{
     config::NUM_SLOTS,
     crypto::{FakeSigner, FilledSlot},
+    responder::AllStates,
 };
 
 #[derive(Copy, Clone, Debug, FromPrimitive)]
@@ -35,14 +36,35 @@ impl From<SpdmError> for u16 {
     }
 }
 
+#[derive(Copy, Clone, PartialEq, Debug)]
+enum State {
+    Error,
+    Version,
+    Capabilities,
+    Algorithms,
+    IdAuth,
+    Challenge,
+}
+
+impl From<&AllStates> for State {
+    fn from(state: &AllStates) -> Self {
+        match state {
+            AllStates::Error => State::Error,
+            AllStates::Version(_) => State::Version,
+            AllStates::Capabilities(_) => State::Capabilities,
+            AllStates::Algorithms(_) => State::Algorithms,
+            AllStates::IdAuth(_) => State::IdAuth,
+            AllStates::Challenge(_) => State::Challenge,
+        }
+    }
+}
+
 /// Record the types and sizes of the messages sent and received by this server
 #[derive(Copy, Clone, PartialEq, Debug)]
 enum LogMsg {
     // Static initializer
     Init,
-    // _Received { code: u8, size: u16 },
-    // _Sent { code: u8, size: u16 },
-    State(&'static str),
+    State(State),
 }
 
 #[export_name = "main"]
@@ -51,7 +73,7 @@ fn main() -> ! {
     const EMPTY_SLOT: Option<FilledSlot<'_, FakeSigner>> = None;
     let slots = [EMPTY_SLOT; NUM_SLOTS];
     let mut responder = spdm::Responder::new(slots);
-    ringbuf_entry!(LogMsg::State(responder.state().name()));
+    ringbuf_entry!(LogMsg::State(responder.state().into()));
 
     let mut buffer = [0; idl::INCOMING_SIZE];
     let mut server = ServerImpl {
@@ -59,7 +81,7 @@ fn main() -> ! {
     };
 
     loop {
-     //   idol_runtime::dispatch(&mut buffer, &mut server);
+        idol_runtime::dispatch(&mut buffer, &mut server);
     }
 }
 
@@ -68,7 +90,8 @@ struct ServerImpl<'a> {
 }
 
 impl idl::InOrderSpdmImpl for ServerImpl<'_> {
-    fn exchange(&mut self,
+    fn exchange(
+        &mut self,
         _: &RecvMessage,
         source: LenLimit<Leased<R, [u8]>, 256>,
         sink: LenLimit<Leased<W, [u8]>, 256>,
@@ -78,7 +101,7 @@ impl idl::InOrderSpdmImpl for ServerImpl<'_> {
 }
 
 mod idl {
-    use super::{SpdmError};
+    use super::SpdmError;
 
     include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
 }
