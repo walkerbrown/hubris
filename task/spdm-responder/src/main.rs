9d32125392afece14eb9f82bf361d2086e3522c6
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-01-31 15:28:16 -0500

    More complete responder impl

--- task/spdm-responder/src/main.rs
+++ task/spdm-responder/src/main.rs
@@ -18,13 +18,13 @@ use spdm::{
 #[derive(Copy, Clone, Debug, FromPrimitive)]
 #[repr(u32)]
 pub enum SpdmError {
-    SomeSortOfError = 1,
+    ResponderReset = 1,
 }
 
 impl From<u32> for SpdmError {
     fn from(x: u32) -> Self {
         match x {
-            1 => SpdmError::SomeSortOfError,
+            1 => SpdmError::ResponderReset,
             _ => panic!(),
         }
     }
@@ -65,20 +65,19 @@ enum LogMsg {
     // Static initializer
     Init,
     State(State),
+    Recv {
+        sender: TaskId,
+        op: u32,
+        msg_len: usize,
+    },
 }
+
+ringbuf!(LogMsg, 16, LogMsg::Init);
 
 #[export_name = "main"]
 fn main() -> ! {
-    ringbuf!(LogMsg, 16, LogMsg::Init);
-    const EMPTY_SLOT: Option<FilledSlot<'_, FakeSigner>> = None;
-    let slots = [EMPTY_SLOT; NUM_SLOTS];
-    let mut responder = spdm::Responder::new(slots);
-    ringbuf_entry!(LogMsg::State(responder.state().into()));
-
     let mut buffer = [0; idl::INCOMING_SIZE];
-    let mut server = ServerImpl {
-        responder: responder,
-    };
+    let mut server = ServerImpl::new();
 
     loop {
         idol_runtime::dispatch(&mut buffer, &mut server);
@@ -89,13 +88,56 @@ struct ServerImpl<'a> {
     responder: spdm::Responder<'a, FakeSigner>,
 }
 
+impl<'a> ServerImpl<'a> {
+    fn new() -> ServerImpl<'a> {
+        const EMPTY_SLOT: Option<FilledSlot<'_, FakeSigner>> = None;
+        let slots = [EMPTY_SLOT; NUM_SLOTS];
+        let responder = spdm::Responder::new(slots);
+        ringbuf_entry!(LogMsg::State(responder.state().into()));
+        ServerImpl { responder }
+    }
+}
+
 impl idl::InOrderSpdmImpl for ServerImpl<'_> {
     fn exchange(
         &mut self,
-        _: &RecvMessage,
+        msg: &RecvMessage,
         source: LenLimit<Leased<R, [u8]>, 256>,
         sink: LenLimit<Leased<W, [u8]>, 256>,
     ) -> Result<(), RequestError<SpdmError>> {
+        ringbuf_entry!(LogMsg::Recv {
+            sender: msg.sender,
+            op: msg.operation,
+            msg_len: msg.message_len,
+        });
+
+        let mut req = [0u8; 256];
+        let mut rsp = [0u8; 256];
+        (*source).read_range(0..source.len(), &mut req).unwrap();
+
+        let (reply, res) =
+            self.responder.handle_msg(&req[..source.len()], &mut rsp);
+        ringbuf_entry!(LogMsg::State(self.responder.state().into()));
+
+        // There was a protocol error. Just go back to the initial state.
+        if res.is_err() {
+            self.responder.reset();
+            ringbuf_entry!(LogMsg::State(self.responder.state().into()));
+
+            // A reply is always valid, even in the case of an error, since we want
+            // to inform the requester of any SPDM errors that should go over the wire. In some
+            // cases, reply may be of zero length however, indicating that nothing should be
+            // sent to the responder and any connections should be closed. In this
+            // case we return an Error.
+            //
+            // TODO: Map actual underlying errors?
+            if reply.is_empty() {
+                return Err(SpdmError::ResponderReset.into());
+            }
+        }
+
+        sink.write_range(0..reply.len(), reply).unwrap();
+
         Ok(())
     }
 }
